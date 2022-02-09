7b7d7f5902a4ac52b5ead65769ee34ec4bbb9b57
compu <edvaldolaurentina07@hotmail.com>
2022-02-09 20:10:01 +0000

    sixth commit

---
+++ src/estruturasCondicionais/Exercicio03.java
@@ -0,0 +1,34 @@
+package estruturasCondicionais;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+
+        /*
+        Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+        Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+        ordem crescente ou decrescente.
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("digite um numero");
+        int numberOne = sc.nextInt();
+
+        System.out.println("digite um segundo numero");
+        int numberTwo = sc.nextInt();
+
+        /*
+        number % 2 == 0 && number % 2 == 0
+        */
+        if( numberOne % 2 == 0 && numberTwo % 2 == 0){
+            System.out.printf("O %d e o %d SÃO MULTIPLOS",numberOne, numberTwo );
+        } else {
+            System.out.printf("O %d e o %d NÃO SÃO MULTIPLOS",numberOne, numberTwo );
+        }
+
+        sc.close();
+
+    }
+}
